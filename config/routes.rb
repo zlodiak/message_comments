475f683c23134f1992381e95836a26fdffd7f245
zlodiak <prozaik81-2@yandex.ru>
2015-10-21 22:31:10 +0300

    auth devise ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/home'
 
   get 'comments/create'
 
   resources :messages
+
+  #get 'persons/profile', as: 'user_root'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
